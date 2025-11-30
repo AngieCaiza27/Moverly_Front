e5ecf9b5fcaf02d43eeaef2409a54b872896f834
ThomXasG <joelvacaross2003@hotmail.com>
2025-11-30 16:48:52 -0500

    Se arregla la página inicial de la aplicación, ahora se mostrará el login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,9 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{ headerShown: false }}
+      initialRouteName="login">
       <Stack.Screen name="login" />
       <Stack.Screen name="register" />
       <Stack.Screen name="forgot-password" />
